fc78c1123fe9a8351d706ae6dbf124d858c90aa2
Benoît Smets <70129050+besmets@users.noreply.github.com>
2024-04-11 20:45:14 +0200

    Add Chunk Duplicator script

---
+++ python_scripts_for_Metashape/AMP210_Chunk_Duplicator.py
@@ -0,0 +1,47 @@
+#-------------------------------------------------------------------------------
+# Name:         AMP210_Chunk_Duplicator.py
+# Purpose:      Duplicate a given chunk multiple times, based on the need of
+#               the user.
+#
+# Compatibility: Agisoft Metashape Pro 2.1.x
+#
+# Version 210 (Compatible with AMP 2.1.0) – 11 April 2024
+#
+# Author:
+#     Benoît Smets
+#     Royal Museum for Central Africa / Vrije Unversiteit Brussel (Belgium)
+#
+# Important Note:   Before use, the name of the chunk to duplicate and the
+#                   number of duplication must be first hard-coded in the
+#                   script.
+#-------------------------------------------------------------------------------
+
+################################################################################
+###############################    SETUP AREA    ###############################
+################################################################################
+
+# Name of the chunk to duplicate
+chunk_name = "Test_name1"
+
+# Number of duplication to perform
+number = 3
+
+###############################   END OF SETUP   ###############################
+################################################################################
+
+print("CHUNK DUPLICATOR")
+print("================")
+print(" ")
+
+import Metashape
+
+doc = Metashape.app.document
+
+for chunk in doc.chunks:
+    if chunk.label == chunk_name:
+        print(f"--> Chunk to duplicate = {chunk_name}")
+        print(' ')
+        for i in range(number):
+            chunk.copy()
+        print(' ')
+        print(f"--> Chunk duplicated {number} times successfully")
